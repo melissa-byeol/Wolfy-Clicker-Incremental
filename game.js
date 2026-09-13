bf6639f93a1b94657c6dbc6eb74b096791c7ec90
melissa-byeol <melissabyeol2012@gmail.com>
2026-09-13 20:38:10 -0300

    Update game.js

--- game.js
+++ game.js
@@ -265,7 +265,12 @@ function clickGalletita() {
     guardarJuego();
     render();
 
-    alert(`¡Nuestros lobitos se comieron la galleta a tiempo! 🍪 Multiplicador x1.5 activo por ${10 + tiempoGanado}s.`);
+    // 💬 MENSAJE PERSONALIZADO SEGÚN EL TIEMPO RESTANTE
+    if (tiempoRestanteExacto <= 3.0) {
+      alert(`¡Esa galleta casi se nos arranca! 🍪💥 Pero lo logramos. ¡Multiplicador x1.5 activo por ${10 + tiempoGanado}s!`);
+    } else {
+      alert(`¡Nuestros lobitos se comieron la galleta a tiempo! 🍪 Multiplicador x1.5 activo por ${10 + tiempoGanado}s.`);
+    }
   }
 }
 function ocultarGalleta() {
